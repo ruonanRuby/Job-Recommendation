acca9df892784eaa3bdf9e70b81d60232db12fbf
Ruonan_Ruby <rniu3@wisc.edu>
2020-10-06 20:24:18 -0500

    Update MySQLDBUtil.java

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "laiproject-instance.cgrtibw0gzbk.us-east-1.rds.amazonaws.com";
+    private static final String INSTANCE = "gitHubExample-instance.cgrtibw0gzbk.us-east-1.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "laiproject";
+    public static final String DB_NAME = "githubExample";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "laiofferproject";
+    private static final String PASSWORD = "githubxxxxxxxxxx";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
